21ebb847cd68eefcdbd283c9b80192aa478dbe2d
kileyerin <kileydowling@gmail.com>
2015-06-03 14:50:56 -0400

    Unit Tests, Loops #1-6

---
+++ Warmups.Loops/Warmups.Loops.BLL/LoopWarmups.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmups.Loops.BLL
+{
+    public class LoopWarmups
+    {
+        public string StringTimes(string str, int n)
+        {
+            string result = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                result += str;
+            }
+
+            return result;
+        }
+
+        public string FrontTimes(string str, int n)
+        {
+            string result = "";
+            char[] tempArray = new char[n * 3];
+            char[] strToArray = str.ToCharArray();
+            int counter = 0;
+
+            //add logic for if string is less than 3 chars
+
+            while (counter < n * 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    tempArray[counter + i] = strToArray[i];
+                }
+                counter += 3;
+            }
+            result = new string(tempArray);
+            return result;
+
+        }
+
+        public int CountXX(string str)
+        {
+            char[] tempArray = str.ToCharArray();
+            int result = 0;
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (tempArray[i] == 'x' && tempArray[i + 1] == 'x')
+                {
+                    result += 1;
+                }
+            }
+            return result;
+        }
+
+        public bool DoubleX(string str)
+        {
+            bool result;
+            int firstX = str.IndexOf('x');
+
+            if (str[firstX + 1] == 'x')
+          
+                result = true;
+            else
+                result = false;
+            return result;
+
+        }
+
+        public string EveryOther(string str)
+        {
+            string result = "";
+            char[] strToArray = str.ToCharArray();
+            int counter = 1;
+            int x = 1;
+            if (str.Length%2 == 0)
+                x = 0;
+            char[] tempArray = new char[str.Length/2+x];
+
+            tempArray[0] = strToArray[0];
+        
+                for (int i = 1; i < tempArray.Length; i++)
+                {
+                    tempArray[i] = strToArray[i + counter];
+                    counter += 1;
+                }
+
+            result = new string(tempArray);
+            return result;
+        }
+
+        public string StringSplosion(string str)
+        {
+            string result ="";
+            int numTimes = 0;
+            char[] strToArray = str.ToCharArray();
+
+
+            while (numTimes < str.Length+1)
+            {
+                for (int i = 0; i < numTimes; i++)
+                {
+                    result += strToArray[i];
+                }
+                numTimes += 1;
+            }
+
+            return result;
+        }
+    }
+
+}
---
+++ Warmups.Loops/Warmups.Loops.Tests/LoopTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Warmups.Loops.BLL;
+
+namespace Warmups.Loops.Tests
+{
+    [TestFixture]
+    public class LoopTests
+    {
+        [TestCase("Hi", 2, "HiHi")]
+        [TestCase("Hi", 3, "HiHiHi")]
+        [TestCase("Hi", 1, "Hi")]
+        [TestCase("Hi", 5, "HiHiHiHiHi")]
+        public void StringTimesTest(string str, int n, string expectedResult)
+        {
+            //Arrange
+            LoopWarmups loops = new LoopWarmups();
+            //Act
+            string actualResult = loops.StringTimes(str, n);
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("Chocolate", 2, "ChoCho")]
+        [TestCase("Chocolate", 3, "ChoChoCho")]
+        [TestCase("Abc", 3, "AbcAbcAbc")]
+//[TestCase("Ab", 3, "AbAbAb")] 
+        public void FrontTimesTest(string str, int n, string expectedResult)
+        {
+            LoopWarmups loop2 = new LoopWarmups();
+            string actualResult = loop2.FrontTimes(str, n);
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+
+        [TestCase("abcxx", 1)]
+        [TestCase("xxx", 2)]
+        [TestCase("xxxx", 3)]
+        public void CountXXTest(string str, int expectedResult)
+        {
+            LoopWarmups loop3 = new LoopWarmups();
+            int actualResult = loop3.CountXX(str);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("axxbb", true)]
+        [TestCase("axaxxax", false)]
+        [TestCase("xxxxx", true)]
+        public void DoubleXTest(string str, bool expectedResult)
+        {
+           LoopWarmups loop4 = new LoopWarmups();
+            bool actualResult = loop4.DoubleX(str);
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+
+        [TestCase("Hello", "Hlo")]
+        [TestCase("Hi", "H")]
+        [TestCase("Heeololeo", "Hello")]
+        public void EveryOtherTest(string str, string expectedResult)
+        {
+            LoopWarmups loop5 = new LoopWarmups();
+            string actualResult = loop5.EveryOther(str);
+            Assert.AreEqual(expectedResult,actualResult);
+
+        }
+
+        [TestCase("Code", "CCoCodCode")]
+        [TestCase("abc", "aababc")]
+        [TestCase("ab", "aab")]
+        public void StringSplosionTest(string str, string expectedResult)
+        {
+            LoopWarmups loop6 = new LoopWarmups();
+            string actualResult = loop6.StringSplosion(str);
+            Assert.AreEqual(expectedResult,actualResult);
+      
+        }
+    }
+}
