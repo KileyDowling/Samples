5a943185f9cf5c753b7fd5d2bba9ac5cee68f023
kileyerin <kileydowling@gmail.com>
2015-06-05 13:10:51 -0400

    String 1-11, in progress

---
+++ Warmups.Strings/Warmups.Strings.BLL/StringWarmups.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmups.Strings.BLL
+{
+    public class StringWarmups
+    {
+        public string SayHi(string name)
+        {
+            string result = "Hi, " + name + "!";
+            return result;
+        }
+
+        public string Abba(string a, string b)
+        {
+            string message = a + b + b + a;
+            return message;
+        }
+
+        public string MakeTags(string tag, string content)
+        {
+            string result="";
+            string tagType = "<" + tag + ">";
+            string closeTag = "</" + tag + ">";
+            result = tagType + content + closeTag;
+            return result;
+        }
+
+        public string insertTheWord(string container, string word)
+        {
+            string result="";
+            int length = container.Length;
+            char[] array = container.ToCharArray();
+
+            string container1 = array[0].ToString() + array[1].ToString();
+            string container2 = array[2].ToString() + array[3].ToString();
+            result = container1 + word + container2;
+
+            return result;
+
+        }
+
+        public string MultEnd(string str)
+        {
+            string result="";
+            int length = str.Length;
+            char[] stringToArray = new char[length];
+            stringToArray = str.ToCharArray();
+            char lastLetter;
+            char secondToLast;
+
+            lastLetter = stringToArray[length - 1];
+            secondToLast = stringToArray[length - 2];
+            string together = secondToLast.ToString() + lastLetter.ToString();
+            result = together + together + together;
+            return result;
+        }
+
+        public string FirstHalf(string str)
+        {
+            string result="";
+            int length = str.Length;
+            int half = length / 2;
+            for (int i = 0; i < half; i++)
+            {
+                result += str[i];
+            }
+            return result;
+        }
+
+        public string TrimOne(string str)
+        {
+            string result="";
+            int length = str.Length;
+            for (int i = 0; i < (length - 2); i++)
+            {
+                result+= str[i + 1];
+            }
+            return result;
+
+        }
+
+        public string LongInMid(string a, string b)
+        {
+            int lengthA = a.Length;
+            int lengthB = b.Length;
+            string result="";
+
+            if (lengthA > lengthB)
+            {
+                result = b + a + b;
+                return result;
+            }
+            else
+            {
+                result = a + b + a;
+                return result;
+            }
+        }
+
+        public string Rotateleft2(string str)
+        {
+            string result ="";
+            int length = str.Length;
+
+            //assign strToArray to tempArray, starting with the 2nd place
+            for (int i = 2; i < (length); i++)
+            {
+                result+= str[i];
+            }
+
+            //then add the first two chars to the end
+            for (int j = (length - 2); j < (length); j++)
+            {
+                result += str[j - length + 2];
+            }
+
+
+            return result;
+        }
+
+
+        public string RotateRight2(string str)
+        {
+            string result="";
+            int length = str.Length;
+
+            for (int i = 0; i < 2; i++)
+            {
+                result += str[i + length - 2];
+            }
+
+            //then add the rest of the char up to the last two
+            for (int j = 2; j < (length); j++)
+            {
+               result += str[j - 2];
+            }
+
+
+            return result;
+        }
+
+        public string TakeOne(string str, bool fromFront)
+        {
+            string result="";
+            int length = str.Length;
+
+            if (fromFront)
+            {
+                char front = str[0];
+                result = front.ToString();
+                return result;
+            }
+            else
+            {
+                char front = str[length - 1];
+                result = front.ToString();
+                return result;
+            }
+
+        }
+
+    }
+}
---
+++ Warmups.Strings/Warmups.Strings.Tests/StringWarmupsTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Warmups.Strings.BLL;
+
+namespace Warmups.Strings.Tests
+{
+    [TestFixture]
+    public class StringWarmupsTests
+    {
+        [TestCase("Bob", "Hi, Bob!")]
+        [TestCase("Alice", "Hi, Alice!")]
+        [TestCase("X", "Hi, X!")]
+        public void SayHiTest(string name, string expectedResult)
+        {
+            StringWarmups string1 = new StringWarmups();
+            string actualResult = string1.SayHi(name);
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+
+        [TestCase("Hi", "Bye", "HiByeByeHi")]
+        [TestCase("Yo", "Alice","YoAliceAliceYo")]
+        [TestCase("What", "Up","WhatUpUpWhat")]
+        public void AbbaTest(string a, string b, string expectedResult)
+        {
+            StringWarmups string2 = new StringWarmups();
+            string actualResult = string2.Abba(a, b);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("i", "Yay", "<i>Yay</i>")]
+        [TestCase("i", "Hello","<i>Hello</i>")]
+        [TestCase("cite", "Yay", "<cite>Yay</cite>")]
+        public void MakeTagsTest(string tag, string content, string expectedResult)
+        {
+            StringWarmups string3 = new StringWarmups();
+            string actualResult = string3.MakeTags(tag,content);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("<<>>", "Yay", "<<Yay>>")]
+        [TestCase("<<>>", "WooHoo", "<<WooHoo>>")]
+        [TestCase("[[]]", "word","[[word]]")]
+        public void insertTheWordTest(string container, string word, string expectedResult)
+        {
+            StringWarmups string4 = new StringWarmups();
+            string actualResult = string4.insertTheWord(container, word);
+            Assert.AreEqual(expectedResult, actualResult);
+            
+        }
+
+        [TestCase("Hello", "lololo")]
+        [TestCase("ab", "ababab")]
+        [TestCase("Hi","HiHiHi")]
+        public void MultEndTest(string str, string expectedResult)
+        {
+            StringWarmups string5 = new StringWarmups();
+            string actualResult = string5.MultEnd(str);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("WooHoo", "Woo")]
+        [TestCase("HelloThere", "Hello")]
+        [TestCase("abcdef", "abc")]
+        public void FirstHalfTest(string str, string expectedResult)
+        {
+            StringWarmups string6 = new StringWarmups();
+            string actualResult = string6.FirstHalf(str);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("Hello", "ell")]
+        [TestCase("java", "av")]
+        [TestCase("coding", "odin")]
+        public void TrimOneTest(string str, string expectedResult)
+        {
+            StringWarmups string7 = new StringWarmups();
+            string actualResult = string7.TrimOne(str);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [TestCase("Hello", "hi", "hiHellohi")]
+        [TestCase("hi", "Hello", "hiHellohi")]
+        [TestCase("aaa", "b", "baaab")]
+        public void LongInMid(string a, string b, string expectedResult)
+        {
+            StringWarmups string8 = new StringWarmups();
+            string actualResult = string8.LongInMid(a, b);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("Hello", "lloHe")]
+        [TestCase("java","vaja")]
+        [TestCase("Hi","Hi")]
+        public void Rotateleft2Test(string str, string expectedResult)
+        {
+            StringWarmups string9 = new StringWarmups();
+            string actualResult = string9.Rotateleft2(str);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("Hello", "loHel")]
+        [TestCase("java", "vaja")]
+        [TestCase("Hi", "Hi")]
+        public void RotateRight2Test(string str, string expectedResult)
+        {
+            StringWarmups string10 = new StringWarmups();
+            string actualResult = string10.RotateRight2(str);
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+
+        [TestCase("Hello", true, "H")]
+        [TestCase("Hello", false, "o")]
+        [TestCase("oh", true, "o")]
+        public void TakeOneTest(string str, bool fromFront, string expectedResult)
+        {
+            StringWarmups string11 = new StringWarmups();
+            string actualResult = string11.TakeOne(str,fromFront);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+    }
+
+}
