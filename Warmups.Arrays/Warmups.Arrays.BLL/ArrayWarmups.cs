1b3bf9cf2f7abae37632849d32cdaf4737841fb5
kileyerin <kileydowling@gmail.com>
2015-06-05 17:01:28 -0400

    Array warmups with unit tests, 1-15

---
+++ Warmups.Arrays/Warmups.Arrays.BLL/ArrayWarmups.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmups.Arrays.BLL
+{
+    public class ArrayWarmups
+    {
+        public bool FirstLast6(int[] numbers)
+        {
+            bool result;
+            int length = numbers.Length;
+            int num6 = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (numbers[i] == 6 && i == 0 || numbers[i] == 6 && i == length - 1)
+                {
+                    num6 += 1;
+                }
+            }
+            if (num6 > 0)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+
+        }
+
+        public bool SameFirstLast(int[] numbers)
+        {
+            bool result;
+            int firLast = 0;
+            int length = numbers.Length;
+
+            if (numbers[0] == numbers[length - 1])
+            {
+                firLast += 1;
+            }
+
+            if (firLast > 0)
+                result = true;
+            else
+                result = false;
+            return result;
+
+        }
+
+        public int[] MakePi(int n)
+        {
+            int[] result = new int[n];
+            int[] piAsArray = {3, 1, 4, 1, 5, 9, 2, 6, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6};
+
+
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = piAsArray[i];
+            }
+
+            return result;
+
+        }
+
+        public bool CommonEnd(int[] a, int[] b)
+        {
+            bool result;
+            int length1 = a.Length;
+            int length2 = b.Length;
+            int samefirLas = 0;
+
+            if (a[0] == b[0] || a[length1 - 1] == b[length2 - 1])
+            {
+                samefirLas += 1;
+            }
+
+            if (samefirLas > 0)
+                result = true;
+            else
+                result = false;
+
+            return result;
+        }
+
+        public int Sum(int[] numbers)
+        {
+            int result = 0;
+            int runningTot = 0;
+            int length = numbers.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                runningTot += numbers[i];
+            }
+            result = runningTot;
+            return result;
+
+        }
+
+        public int[] RotateLeft(int[] numbers)
+        {
+            int length = numbers.Length;
+            int[] result = new int[length];
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                result[i] = numbers[i + 1];
+            }
+
+            result[length - 1] = numbers[0];
+
+            return result;
+        }
+
+        public int[] Reverse(int[] numbers)
+        {
+            int length = numbers.Length - 1;
+            int[] result = new int[length + 1];
+            int counter = 0;
+
+            for (int i = length; i >= 0; i--)
+            {
+                result[counter] = numbers[i];
+                counter += 1;
+            }
+            return result;
+
+        }
+
+        public int[] HigherWins(int[] numbers)
+        {
+            int length = numbers.Length;
+            int[] result = new int[length];
+            int isLarger;
+
+            if (numbers[0] > numbers[length - 1])
+                isLarger = numbers[0];
+            else
+                isLarger = numbers[length - 1];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = isLarger;
+            }
+
+            return result;
+        }
+
+        public int[] GetMiddle(int[] a, int[] b)
+        {
+            int[] result = new int[2];
+
+            result[0] = a[1];
+            result[1] = b[1];
+
+            return result;
+        }
+
+
+        public bool HasEven(int[] numbers)
+        {
+            bool result;
+            int length = numbers.Length;
+            int hasEv = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (numbers[i]%2 == 0)
+                {
+                    hasEv += 1;
+                }
+            }
+
+            if (hasEv > 0)
+                result = true;
+            else
+                result = false;
+
+            return result;
+        }
+
+        public int[] KeepLast(int[] numbers)
+        {
+            int length = numbers.Length*2;
+            int[] result = new int[length];
+
+            result[length - 1] = numbers[numbers.Length - 1];
+
+            return result;
+        }
+
+        public bool Double23(int[] numbers)
+        {
+            bool result;
+            int length = numbers.Length;
+            int doub2 = 0;
+            int doub3 = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (numbers[i] == 2)
+                {
+                    doub2 += 1;
+                }
+                else if (numbers[i] == 3)
+                {
+                    doub3 += 1;
+                }
+            }
+
+            if (doub2 == 2 || doub3 == 2)
+                result = true;
+            else
+                result = false;
+
+            return result;
+        }
+
+
+        public int[] Fix23(int[] numbers)
+        {
+            int length = numbers.Length;
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                if (numbers[i] == 2 && numbers[i + 1] == 3)
+                    numbers[i + 1] = 0;
+            }
+
+            return numbers;
+        }
+
+        public bool Unlucky1(int[] numbers)
+        {
+            bool result;
+            int length = numbers.Length;
+            int foundUn1 = 0;
+
+            if (numbers[0] == 1 && numbers[1] == 3 || numbers[1] == 1 && numbers[2] == 3 ||
+                numbers[length - 2] == 1 && numbers[length - 1] == 3)
+            {
+                foundUn1 += 1;
+            }
+            if (foundUn1 > 0)
+                result = true;
+            else
+                result = false;
+
+            return result;
+        }
+
+        public int[] make2(int[] a, int[] b)
+        {
+            int[] result = new int[2];
+            int lengthA = a.Length;
+            int counter = 0;
+
+            while (counter < lengthA)
+            {
+                result[counter] = a[counter];
+                counter += 1;
+            }
+
+            if (2 > lengthA)
+            {
+                for (int j = lengthA; j < 2; j++)
+                {
+                    result[j] = b[j - counter];
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ Warmups.Arrays/Warmups.Arrays.Tests/ArrayTests.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Warmups.Arrays.BLL;
+namespace Warmups.Arrays.Tests
+{
+    [TestFixture]
+    public class ArrayTests
+    {
+        [TestCase(new int[] {1, 2, 6}, true)]
+        [TestCase(new int[] {6, 1, 2, 3}, true)]
+        [TestCase(new int[] {13, 6, 1, 2, 3}, false)]
+        public void FirstLast6Test(int[] numbers, bool expectedResult)
+        {
+            ArrayWarmups array1 = new ArrayWarmups();
+            bool actualResult = array1.FirstLast6(numbers);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase(new int[] {1, 2, 3}, false)]
+        [TestCase(new int[] {1, 2, 3, 1}, true)]
+        [TestCase(new int[] {1, 2, 1}, true)]
+        public void SameFirstLastTest(int[] numbers, bool expectedResult)
+        {
+            ArrayWarmups array2 = new ArrayWarmups();
+            bool actualResult = array2.SameFirstLast(numbers);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase((3), new int[] {3, 1, 4})]
+        [TestCase((1), new int[] {3})]
+        public void MakePiTest(int n, int[] expectedResult)
+        {
+            ArrayWarmups array3 = new ArrayWarmups();
+            int[] actualResult = array3.MakePi(n);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase(new int[] {1, 2, 3}, new int[] {7, 3}, true)]
+        [TestCase(new int[] {1, 2, 3}, new int[] {7, 3, 2}, false)]
+        [TestCase(new int[] {1, 2, 3}, new int[] {1, 3}, true)]
+        public void CommonEndTest(int[] a, int[] b, bool expectedResult)
+        {
+            ArrayWarmups array4 = new ArrayWarmups();
+            bool actualResult = array4.CommonEnd(a, b);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase(new int[] {1, 2, 3}, 6)]
+        [TestCase(new int[] {5, 11, 2}, 18)]
+        [TestCase(new int[] {7, 0, 0}, 7)]
+        public void SumTest(int[] numbers, int expetedResult)
+        {
+            ArrayWarmups array5 = new ArrayWarmups();
+            int actualResult = array5.Sum(numbers);
+            Assert.AreEqual(expetedResult, actualResult);
+        }
+
+        [TestCase(new int[] {1, 2, 3}, new int[] {2, 3, 1})]
+        [TestCase(new int[] {5, 11, 9}, new int[] {11, 9, 5})]
+        [TestCase(new int[] {7, 0, 0}, new int[] {0, 0, 7})]
+        public void RotateLeft(int[] numbers, int[] expectedResult)
+        {
+            ArrayWarmups array6 = new ArrayWarmups();
+            int[] actualResult = array6.RotateLeft(numbers);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 3, 2, 1 })]
+        public void ReverseTest(int[] numbers, int[] expectedResults)
+        {
+            ArrayWarmups array7 = new ArrayWarmups();
+            int[] actualResult = array7.Reverse(numbers);
+            Assert.AreEqual(expectedResults,actualResult);
+        }
+
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 3, 3, 3 })]
+        [TestCase(new int[] { 11, 5, 9 }, new int[] { 11, 11, 11 })]
+        [TestCase(new int[] { 2, 11, 3 }, new int[] { 3,3,3 })]
+        public void HigherWinsTest(int[] numbers, int[] expectedResults)
+        {
+            ArrayWarmups array8 = new ArrayWarmups();
+            int[] actualResult = array8.HigherWins(numbers);
+            Assert.AreEqual(expectedResults,actualResult);
+        }
+
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, new int[] { 2, 5 })]
+        [TestCase(new int[] { 7, 7, 7 }, new int[] { 3,8,0 }, new int[] { 7,8 })]
+        [TestCase(new int[] {5,2,9 }, new int[] {1, 4, 5 }, new int[] { 2, 4})]
+        public void GetMiddleTest(int[] a, int[] b, int[] expectedResult)
+        {
+            ArrayWarmups array9 = new ArrayWarmups();
+            int[] actualResult = array9.GetMiddle(a, b);
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+
+        [TestCase(new int[] { 2, 5 }, true)]
+        [TestCase(new int[] { 4,3 }, true)]
+        [TestCase(new int[] { 7, 5 }, false)]
+        public void HasEvenTest(int[] numbers, bool expectedResult)
+        {
+            ArrayWarmups array10 = new ArrayWarmups();
+            bool actualResult = array10.HasEven(numbers);
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+
+        [TestCase(new int[] { 4, 5, 6 }, new int[] { 0, 0, 0, 0, 0, 6 })]
+        [TestCase(new int[] { 1, 2 }, new int[] { 0, 0, 0, 2 })]
+        [TestCase(new int[] { 3 }, new int[] { 0,3 })]
+        public void KeepLastTest(int[] numbers, int[] expectedResult)
+        {
+            ArrayWarmups array11 = new ArrayWarmups();
+            int[] actualResult = array11.KeepLast(numbers);
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+
+        [TestCase(new int[] { 2, 2, 3 }, true)]
+        [TestCase(new int[] { 3, 4, 5, 3 }, true)]
+        [TestCase(new int[] { 2, 3, 2, 2 }, false)]
+        public void Double23Test(int[] numbers, bool expectedResult)
+        {
+            ArrayWarmups array12 = new ArrayWarmups();
+            bool actualResult = array12.Double23(numbers);
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 2, 0 })]
+        [TestCase(new int[] { 2, 3, 5 }, new int[] {2, 0, 5})]
+        [TestCase(new int[] { 1, 2, 1}, new int[] { 1, 2, 1})]
+        public void Fix23Test(int[] numbers, int[] expectedResult)
+        {
+            ArrayWarmups array13 = new ArrayWarmups();
+            int[] actualResult = array13.Fix23(numbers);
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+
+
+        [TestCase(new int[] { 1, 3, 4, 5 }, true)]
+        [TestCase(new int[] { 2, 1, 3, 4, 5 }, true)]
+        [TestCase(new int[] { 1,1,1 }, false)]
+        public void Unlucky1Test(int[] numbers, bool expectedResult)
+        {
+            ArrayWarmups array14 = new ArrayWarmups();
+            bool actualResult = array14.Unlucky1(numbers);
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+
+        [TestCase(new int[] { 4, 5 }, new int[] { 1, 2, 3 }, new int[] { 4, 5 })]
+        [TestCase(new int[] { 4}, new int[] { 1, 2, 3 }, new int[] { 4, 1 })]
+        [TestCase(new int[] { }, new int[] { 1, 2}, new int[] { 1,2 })]
+        public void make2Test(int[] a, int[] b, int[] expectedResult)
+        {
+            ArrayWarmups array15 = new ArrayWarmups();
+            int[] actualResult = array15.make2(a, b);
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+
+
+
+
+
+
+
+}
+}
