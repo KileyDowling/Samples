9d827c1c4cc70f49c97462443652ee37d899a642
kileyerin <kileydowling@gmail.com>
2015-06-08 11:30:01 -0400

    Die exercise

---
+++ DieExercise/DieExercise/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng1 = new Random();
+            int[] result = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                result[i] = rng1.Next(1, 13);
+            }
+
+            int foundNum = 0;
+            int count = 0;
+
+            int total = 0;
+
+
+
+            for (int i = 2; i < 13; i++)
+            {
+                foreach (int num in result)
+                {
+                    if (result[num] == i)
+                    {
+                        foundNum += 1;
+                    }
+
+                }
+                total += foundNum;
+                Console.WriteLine("Dice Total " + i + "\nTimes Rolled: " + foundNum + "\n");
+                foundNum = 0;
+            }
+            Console.WriteLine("total rolls:" + total);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
