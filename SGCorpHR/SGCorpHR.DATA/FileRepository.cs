6cce3871999141c2a80415b0e2bed32e7813237e
kileyerin <kileydowling@gmail.com>
2015-07-01 11:36:12 -0400

    Updates

---
+++ SGCorpHR/SGCorpHR.DATA/FileRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SGCorpHR.Models;
+
+namespace SGCorpHR.DATA
+{
+   public class FileRepository
+   {
+       public List<Resumes> GetFiles()
+       {
+           var resume = new Resumes();
+           var directory = new DirectoryInfo(@"C:\Users\Apprentice\Desktop\GitHub\KileyDowling\SGCorpHR\SGCorpHR.UI\Resumes");
+           var files = directory.GetFiles();
+           if (files.Any())
+           {
+               var resumes = new List<Resumes>();
+               foreach (var file in files)
+               {
+                   resume.FilePath = file.FullName;
+                   resume.FileName = file.Name;
+                   resumes.Add(resume);
+               }
+
+               return resumes;
+           }
+           return null;
+
+       }
+   }
+}
---
+++ SGCorpHR/SGCorpHR.UI/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SGCorpHR.BLL;
+using SGCorpHR.Models;
+using System.IO;
+using System.Net;
+
+namespace SGCorpHR.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var ops = new FileOperations();
+            var response = ops.DisplayFiles();
+            return View(response);
+        }
+
+        public ActionResult Save(FormCollection formCollection)
+        {
+            if (Request != null)
+            {
+                HttpPostedFileBase file = Request.Files["UploadedFile"];
+
+                if((file != null) && (file.ContentLength > 0) && !String.IsNullOrEmpty(file.FileName))
+                {
+                    string fileName = file.FileName;
+                    string fileContentType = file.ContentType;
+                    byte[] fileBytes = new byte[file.ContentLength];
+                    file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+
+                }
+            }
+
+            return View("Index");
+
+        }
+    }
+}
