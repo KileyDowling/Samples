7355bbc9b7cf95b01f54f64689e059422bc135bc
kileyerin <kileydowling@gmail.com>
2015-07-02 14:46:52 -0400

    Finished

--- SGCorpHR/SGCorpHR.UI/App_Start/RouteConfig.cs
+++ SGCorpHR/SGCorpHR.UI/App_Start/RouteConfig.cs
@@ -13,11 +13,21 @@ namespace SGCorpHR.UI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapRoute(
+                name: "Suggestion",
+                url: "Suggestion",
+                defaults: new { controller = "Home", action = "AddSuggestion" }
+                );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+                );
+
+            
+
         }
     }
 }
