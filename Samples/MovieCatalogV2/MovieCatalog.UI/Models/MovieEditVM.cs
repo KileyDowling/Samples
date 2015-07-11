ecc4d2e87f87ba835cdf6a229ce76b3ecfe9b562
kileyerin <kileydowling@gmail.com>
2015-07-11 18:00:54 -0400

    Attempt to further build out the edit function.

---
+++ Samples/MovieCatalogV2/MovieCatalog.UI/Models/MovieEditVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieCatalog.Models;
+
+namespace MovieCatalog.UI.Models
+{
+    public class MovieEditVM
+    {
+        public Movie MovieToEdit { get; set; }
+        public List<Actor> ActorsInMovie { get; set; }
+        public List<SelectListItem> ActorsNotInMovie { get; set; }
+        public List<SelectListItem> Genres { get; set; }
+        public List<int> SelectedActors { get; set; }
+
+        public void CreateGenreList(List<Genre> genres)
+        {
+            Genres =new List<SelectListItem>();
+            foreach (var g in genres)
+            {
+                Genres.Add(new SelectListItem() {Text = g.GenreName, Value = g.GenreID.ToString()});
+            }   
+        }
+
+        public void CreateActorsNotInMovie(List<Actor> actors)
+        {
+            ActorsNotInMovie = new List<SelectListItem>();
+            foreach (var a in actors)
+            {
+                ActorsNotInMovie.Add(
+                    new SelectListItem() {Text = string.Format("{0}, {1}",a.LastName, a.FirstName), Value=a.ActorID.ToString()}
+                    );
+            }
+        }
+    }
+}
