36fa3bc702409251294afd68661e13ce507823ec
KileyDowling <KileyDowling@gmail.com>
2015-07-23 11:39:02 -0400

    MVC with jQuery validation

---
+++ Samples/MVC_RSVP/MVC_RSVP.UI/Scripts/app/rsvpValidation.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    jQuery.validator.addMethod("Name", function(value, element) {
+        if ((element.value % 4) === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }, "Not an incremenet of 4");
+
+    $('#rsvpForm').validate({
+        rules: {
+            Name: {
+                required: true
+            },
+            Email: {
+                required: true,
+                email: true
+            },
+            Phone: {
+                required: true
+            },
+            FavoriteGame: {
+                required: true
+            },
+            WillAttend: {
+                required: true
+            }
+        },
+            messages: {
+                Name: "Enter your name",
+                Email: {
+                    required: "Enter your email address",
+                    email: "That's not a format for email I'm aware of..."
+                }
+            }
+        
+    });
+})
