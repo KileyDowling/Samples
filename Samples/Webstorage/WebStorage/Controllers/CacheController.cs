51667076083117a60ad93d336983c6f0b02a8628
KileyDowling <KileyDowling@gmail.com>
2015-07-28 16:35:04 -0400

    Webstorage

---
+++ Samples/Webstorage/WebStorage/Controllers/CacheController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+using WebStorage.Models;
+
+namespace WebStorage.Controllers
+{
+    public class CacheController : Controller
+    {
+
+        public ActionResult ReadCache()
+        {
+            List<Contact> contacts = new List<Contact>();
+
+            if (HttpRuntime.Cache["ContactList"] == null)
+            {
+                contacts = new List<Contact>()
+                {
+                    new Contact() {Email = "jo@sm.com", Name = "Joe"},
+                    new Contact() {Email = "alice@sm.com", Name = "Alice"}
+                };
+
+                HttpRuntime.Cache.Insert("ContactList", contacts, null, DateTime.Now.AddHours(8),
+                    Cache.NoSlidingExpiration);
+            }
+            else
+            {
+                contacts = (List<Contact>) HttpRuntime.Cache["ContactList"];
+
+            }
+            
+
+            return View(contacts);
+        }
+
+    }
+}
---
+++ Samples/Webstorage/WebStorage/Controllers/TempDataController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebStorage.Models;
+
+namespace WebStorage.Controllers
+{
+    public class TempDataController : Controller
+    {
+        //
+        // GET: /TempData/
+
+        public ActionResult WriteTempData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult WriteTempData(Contact contact)
+        {
+            TempData["ContactInformation"] = contact;
+            TempData["SuccessMessage"] = "Great job";
+
+            return RedirectToAction("ReadTempData");
+        }
+
+        public ActionResult ReadTempData()
+        {
+            Contact contact = new Contact();
+
+            if (TempData["ContactInformation"] != null)
+            {
+                contact = (Contact)TempData["ContactInformation"];
+            }
+            else
+            {
+                contact = new Contact() { Name = "No TempData", Email = "Fail!" };
+            }
+            //string msg;
+            //if (TempData["SuccessMessage"] != null)
+            //{
+            //    msg = (string)TempData["SuccessMessage"];
+            //}
+            //else
+            //{
+            //    msg = "No message";
+            //}
+
+            return View(contact);
+        }
+
+    }
+}
