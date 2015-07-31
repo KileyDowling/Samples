1c8779ee6ffdddf36552310454b55d30d9265be8
KileyDowling <KileyDowling@gmail.com>
2015-07-30 20:15:00 -0400

    eod

--- SGCorpHRV2/HRPortalKD/SGCorpHR.BLL/EmployeeDirectoryOperations.cs
+++ SGCorpHRV2/HRPortalKD/SGCorpHR.BLL/EmployeeDirectoryOperations.cs
@@ -39,5 +39,100 @@ namespace SGCorpHR.BLL
             return response;
         }
 
+        public Response<List<Departments>> ListAllDepartments()
+        {
+            var repo = new DepartmentRepository();
+            var response = new Response<List<Departments>>();
+            List<Departments> dptList = repo.ListAll();
+
+            try
+            {
+                if (dptList.Count > 0)
+                {
+                    response.Data = dptList;
+                    response.Success = true;
+
+                }
+                else
+                {
+                    response.Success = false;
+                    response.Message = "There  are no employees to display";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                response.Success = false;
+                response.Message = ex.Message;
+            }
+            return response;
+
+
+        }
+
+        public Response<Departments> GetSingleDpt(int departmentId)
+        {
+            var repo = new DepartmentRepository();
+            var response = new Response<Departments>();
+            Departments dpt = repo.GetSingleDpt(departmentId);
+
+            try
+            {
+                if (dpt != null)
+                {
+                    response.Data = dpt;
+                    response.Success = true;
+
+                }
+                else
+                {
+                    response.Success = false;
+                    response.Message = "That department doesn't exist";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                response.Success = false;
+                response.Message = ex.Message;
+            }
+            return response;
+        }
+
+        public Response<List<Employee>> GetEmpByDptID(int departmentId)
+        {
+            var repo = new EmployeeDirectoryRepository();
+            var response = new Response<List<Employee>>();
+            List<Employee> empList = repo.GetEmpByDptID(departmentId);
+
+            try
+            {
+                if (empList != null)
+                {
+                    response.Data = empList;
+                    response.Success = true;
+
+                }
+                else
+                {
+                    response.Success = false;
+                    response.Message = "That department doesn't exist";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                response.Success = false;
+                response.Message = ex.Message;
+            }
+            return response;
+        }
+
+        public void CreateDepartment(string dptName)
+        {
+            var repo = new DepartmentRepository();
+            repo.CreateDepartment(dptName);
+        }
+
     }
 }
---
+++ SGCorpHRV2/HRPortalKD/SGCorpHR.TEST/DepartmentOpsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SGCorpHR.BLL;
+using SGCorpHR.DATA;
+
+namespace SGCorpHR.TEST
+{
+    [TestFixture]
+    public class DepartmentOpsTests
+    {
+        [Test]
+        public void CreateDptTest()
+        {
+            var ops = new EmployeeDirectoryOperations();
+            ops.CreateDepartment("Customer Service");
+            var ptoList = ops.ListAllDepartments();
+            var newDpt = ptoList.Data.FirstOrDefault(x => x.DepartmentID == 8);
+            Assert.AreEqual("Customer Service", newDpt.DepartmentName);
+        }
+
+        [Test]
+        public void ListAllDptTest()
+        {
+            var ops = new EmployeeDirectoryOperations();
+            var response = ops.ListAllDepartments();
+            Assert.IsTrue(response.Success);
+            Assert.AreEqual(8,response.Data.Count);
+        }
+
+        [Test]
+        public void GetSingleDptTest()
+        {
+            var ops = new EmployeeDirectoryOperations();
+            var response = ops.GetSingleDpt(5);
+            Assert.IsTrue(response.Success);
+            Assert.AreEqual("Human Resources", response.Data.DepartmentName);
+        }
+    }
+}
